43f4a4178a36fd4914dfe2c59a94aaf0c4989cbb
Jake <iloveheat1105@gmail.com>
2025-10-19 21:52:25 -0400

    feat: modified datatype for created_at attribute in ORM classes, added basic implementation for db connection

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 import os
 from dotenv import load_dotenv
 
-
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL") 
+engine = create_engine(DATABASE_URL)
 
     
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import String, ForeignKey, NUMERIC, TIMESTAMP, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from typing import List, Optional
-
+from datetime import datetime
 class Base(DeclarativeBase):
     pass
 
@@ -12,7 +12,7 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
-    created_at: Mapped = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
 
     products: Mapped[List["Product"]] = relationship(back_populates="user", cascade="save-update")
 
@@ -23,7 +23,7 @@ class Product(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     name: Mapped[str] = mapped_column(String(255),nullable=False)
     category: Mapped[Optional[str]] = mapped_column(String(100))
-    created_at: Mapped = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
 
     user: Mapped["User"] = relationship(back_populates="products")
     prices: Mapped[List["Price"]] = relationship(back_populates="product", cascade="all, delete-orphan")
@@ -35,7 +35,7 @@ class Price(Base):
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
     price: Mapped[float] = mapped_column(NUMERIC(10,2), nullable=False)
     currency: Mapped[str] = mapped_column(String(10), default="USD")
-    created_at: Mapped = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
 
     product: Mapped["Product"] = relationship(back_populates="prices")
 
