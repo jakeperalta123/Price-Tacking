00e64acac4925158f233eb45a8723b6ba1e8b3ec
Jake <iloveheat1105@gmail.com>
2026-01-19 22:41:11 -0500

    feat: setting up redis fundamental functions and establishing connections with FastAPI

---
+++ backend/app/core/redis.py
@@ -0,0 +1,28 @@
+import redis.asyncio as redis
+import json
+from decimal import Decimal
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super(DecimalEncoder, self).default(obj)
+
+class RedisCache:
+    def __init__(self):
+        self.redis = None
+    
+    async def connect(self):
+        self.redis = await redis.from_url("redis://localhost:6379", decode_response=True)
+
+    async def get_cache(self, key: str):
+        data = await self.redis.get(key)
+        return json.loads(data) if data else None   
+    
+    async def set_cache(self, key: str, value: any, expire: int = 3600):
+        await self.redis.set(key, json.dumps(value, cls=DecimalEncoder), ex=expire)
+
+    async def delete_cache(self, key: str):
+        await self.redis.delete(key)
+
+redis_client = RedisCache()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
 from app.routers import user_router, auth_router, product_router, price_router
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.redis import redis_client
+from contextlib import asynccontextmanager
 
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Connecting to Redis")
+    await redis_client.connect()
+    yield
+    print("Closing Redis connection")
+    if redis_client.redis:
+        await redis_client.redis.close()
+
 app = FastAPI(
     title="Price Tracking API",
     version="1.0.0",
     description="API for price tracking",
+    lifespan=lifespan
 )
 
 app.add_middleware(
